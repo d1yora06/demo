863fb1676d6c739f1e6f30ae51f6c24c48b683dc
d1yora06 <diyorax761@gmail.com>
2023-03-26 15:31:13 +0500

    fix

---
+++ index.js
@@ -0,0 +1,18 @@
+// let a = +prompt("")
+
+// for(let i = 0; i <= a; i++){
+//     console.log(i);
+// }
+
+
+function girls (mavzuna, malika, diyora, muxlisa){
+    if (2000 <= mavzuna + malika + diyora + muxlisa){
+        return "pulingiz yetadi"
+        
+    }else{
+        return "pulingiz yetmaydi"
+    }
+}
+
+
+console.log(girls (50, 50, 50, 50));
